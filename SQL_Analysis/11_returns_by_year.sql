7f5cb26e45b3176c5aefd936001583a3fe2c7bd5
João Montez <joaopro.coelho2026@gmail.com>
2026-04-23 14:54:55 +0100

    Update 11_returns_by_year.sql

--- SQL_Analysis/11_returns_by_year.sql
+++ SQL_Analysis/11_returns_by_year.sql
@@ -1,1 +1,18 @@
+-- ==================================================
+-- Question 11
+-- Are product returns increasing or decreasing over time?
+-- ==================================================
 
+SELECT
+    Return_Year AS Year,
+    SUM(ReturnQuantity) AS TotalReturns
+FROM Analytics.Vw_Fact_Returns
+GROUP BY
+    Return_Year
+ORDER BY
+    Year;
+
+-- Insight:
+-- Returns are clearly increasing, rising from 348
+-- in 2022 to 1,062 in 2024. The upward trend signals
+-- a need to review product quality or return policies.
