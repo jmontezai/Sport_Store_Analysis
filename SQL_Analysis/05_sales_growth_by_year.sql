1e64b8eda68ceea3e1685ce124ba50b3fa9a86e8
João Montez <joaopro.coelho2026@gmail.com>
2026-04-23 14:35:15 +0100

    Update 05_sales_growth_by_year.sql

--- SQL_Analysis/05_sales_growth_by_year.sql
+++ SQL_Analysis/05_sales_growth_by_year.sql
@@ -1,1 +1,21 @@
+-- ==========================================
+-- Question 5
+-- What is the sales growth by year?
+-- ==========================================
 
+SELECT
+    Sales_Year AS Year,
+    SUM(OrderQuantity) AS TotalProductsSold,
+    CAST((SUM(OrderQuantity) - LAG(SUM(OrderQuantity)) OVER (ORDER BY Sales_Year)) * 100.0 
+    / LAG(SUM(OrderQuantity)) OVER (ORDER BY Sales_Year) AS DECIMAL(5,2)) AS GrowthPercent
+FROM Analytics.Vw_Fact_Sales
+GROUP BY
+    Sales_Year
+ORDER BY
+    Year;
+
+-- Insight:
+-- Sales grew consistently from 20.6k units in 2022
+-- to 35.7k in 2024. The biggest jump was in 2023
+-- (+47%), followed by a smaller rise in 2024 (+18%),
+-- showing solid growth at a stabilizing pace.
